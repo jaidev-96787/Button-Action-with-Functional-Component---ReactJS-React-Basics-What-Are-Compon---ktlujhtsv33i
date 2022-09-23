6072aecc589b508b2f6f14f6d4cc1b04743a9a8b
salman3009 <salman444g@gmail.com>
2022-09-23 12:28:33 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-
+import { useState } from 'react';
 function App() {
+
+  const [getFlag,setFlag] = useState(false);
+
+  const onClickHandler=()=>{
+    setFlag(true);
+  }
+
   return (
     <div id="main">
-      // Do not alter the main div
+       <button id="click" onClick={onClickHandler}>Show</button>
+     {getFlag && <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
