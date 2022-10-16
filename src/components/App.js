24d584a796c8839287cd0a89a6ed4d316da0ea66
salman3009 <salman444g@gmail.com>
2022-10-16 10:17:11 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-import { useState } from 'react';
-function App() {
 
-  const [getFlag,setFlag] = useState(false);
+function App() 
+{
+  const [displayMessage, setDisplayMessage] = useState(false);
 
-  const onClickHandler=()=>{
-    setFlag(true);
-  }
-
+  
   return (
-    <div id="main">
-       <button id="click" onClick={onClickHandler}>Show</button>
-     {getFlag && <p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+	<div id="main">
+		{
+    		(displayMessage === false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+    	}
+		<button id="click" onClick={()=>setDisplayMessage(true)}>Click me</button>
     </div>
   );
 }
